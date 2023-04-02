848821db1d9f4fe8099c4a80a2e3c737259302d0
jaeochoiii <jaeochoiii0716@inha.edu>
2023-04-02 17:55:31 +0900

    1번과 5번 작성

---
+++ Problem1.js
@@ -0,0 +1,23 @@
+// 1번 문제.
+
+let name = "Choi";
+
+// ? 부분에 예측 값을 작성해주세요(코드 실행 금지!!!)
+
+alert(`hello ${1}`); // ?(답 작성칸)
+
+alert(`hello ${"name"}`); // ?(답 작성칸)
+
+alert("hello ${name}"); // ?(답 작성칸)
+
+alert(`hello ${name}`); // ?(답 작성칸)
+
+/*
+alert(`hello ${1}`); // 1
+
+alert(`hello ${"name"}`); // hello name
+
+alert("hello ${name}"); // hello ${name}
+
+alert(`hello ${name}`); // hello Choi
+*/
---
+++ Problem5.js
@@ -0,0 +1,17 @@
+// 5번 문제.
+
+let user = {
+  name: "Lion Kim",
+  age: 21,
+};
+
+// 아래에 코드를 작성해주세요~
+
+/*
+let user = {
+  name: "Lion Kim",
+  age: 21
+};
+
+let user2 = JSON.parse(JSON.stringify(user));
+*/
