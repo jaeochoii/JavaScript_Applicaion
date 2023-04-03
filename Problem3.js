6d982a17b7eb21cf6204c5476fd2f4eca8e48f80
jaeochoiii <jaeochoiii0716@inha.edu>
2023-04-03 20:05:43 +0900

    최종

--- Problem3.js
+++ Problem3.js
@@ -1,17 +1,17 @@
 // 3번 문제.
 let userName = prompt("사용자 이름을 입력해주세요.", "");
 
-if (userName == "Admin") {
+if (userName === "Admin") {
   let pass = prompt("비밀번호:", "");
 
-  if (pass == "TheMaster") {
+  if (pass === "TheMaster") {
     alert("환영합니다!");
-  } else if (pass == "" || pass == null) {
+  } else if (pass === "" || pass === null) {
     alert("취소되었습니다.");
   } else {
     alert("인증에 실패하였습니다.");
   }
-} else if (userName == "" || userName == null) {
+} else if (userName === "" || userName === null) {
   alert("취소되었습니다.");
 } else {
   alert("인증되지 않은 사용자입니다.");
